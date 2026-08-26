e5e0773bf388a79749f1fa2efac026dd5761ae4e
LPolic <48826059+PolicLL@users.noreply.github.com>
2026-08-26 13:46:39 +0200

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> temp = new ArrayDeque<>();
+        Deque<Integer> indexes = new ArrayDeque<>();
+
+        temp.push(temperatures[0]);
+        indexes.push(0);
+
+        int[] result = new int[temperatures.length];
+
+        // [30
+        // [0
+        // 
+        //
+        //
+
+        for (int i = 1; i < temperatures.length; ++i) {
+            int val = temperatures[i];
+
+
+            while (!temp.isEmpty() && val > temp.peek()) {
+                int index = indexes.pop();
+                result[index] = i - index;
+                temp.pop();
+            }
+
+            temp.push(val);
+            indexes.push(i);
+        }
+        
+
+        return result;
+    }
+
+    public record Temp(int value, int index) {}
+}
