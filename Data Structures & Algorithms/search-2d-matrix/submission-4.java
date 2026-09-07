d23064b83c1056ed036fe7f682483abf9cd827e6
LPolic <48826059+PolicLL@users.noreply.github.com>
2026-09-07 10:11:37 +0200

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int start = 0, end = matrix.length - 1, row = 0;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            int midVal = matrix[mid][0];
+            row = mid;
+            if (midVal == target) return true;
+            if (target > midVal && (mid + 1) < matrix.length && 
+                matrix[mid + 1][0] > target) {
+                    break;
+                }
+            else if(target < midVal) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return binary(matrix[row], target) != -1 ? true : false;
+
+
+    }
+
+    private int binary(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] > target) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return -1;
+    }
+}
